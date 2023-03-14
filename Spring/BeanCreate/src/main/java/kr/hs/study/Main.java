e3d77df7690036a7ff1106707b69fcfa90e97654
JAYUN <83991079+chojangg@users.noreply.github.com>
2023-03-14 11:37:20 +0900

    BeanCreate

---
+++ Spring/BeanCreate/src/main/java/kr/hs/study/Main.java
@@ -0,0 +1,22 @@
+package kr.hs.study;
+
+import kr.hs.study.beans.TestBean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+
+        TestBean obj1 = ctx.getBean("t1", TestBean.class);
+        System.out.println("obj1: "+obj1);
+
+        System.out.println("---------------------------------------------");
+        TestBean obj2 = ctx.getBean("t3", TestBean.class);
+        System.out.println("obj2: "+obj2);
+
+
+
+        ctx.close();
+        System.out.println("Hello world!");
+    }
+}
